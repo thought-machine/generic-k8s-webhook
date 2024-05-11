7b55c89b9b880607ac705cca69a35fc8481c0745
Jordi Pique <jordipiqueselles@gmail.com>
2024-05-11 19:51:22 +0100

    Refactor the jsonpatch tests
    The jsonpatch tests now follow a similar format as the conditions tests,
    where we define them in a declarative way in a yaml format. Moreover, we
    define tests for specific schema versions. This allows us to keep
    running old tests for old schemas and for schemas with backward
    compatibility and introduce new tests that are only executed for the new
    schemas.

--- tests/jsonpatch_test.py
+++ tests/jsonpatch_test.py
@@ -7,11 +7,11 @@ from test_utils import expand_schemas
 from generic_k8s_webhook.config_parser.entrypoint import GenericWebhookConfigManifest
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-CONDITIONS_YAML = os.path.join(SCRIPT_DIR, "conditions_test.yaml")
+JSONPATCH_YAML = os.path.join(SCRIPT_DIR, "jsonpatch_test.yaml")
 
 
 def _parse_tests() -> list[tuple]:
-    with open(CONDITIONS_YAML, "r") as f:
+    with open(JSONPATCH_YAML, "r") as f:
         raw_tests = yaml.safe_load(f)
 
     parsed_tests = []
@@ -23,22 +23,23 @@ def _parse_tests() -> list[tuple]:
                         (
                             f"{test_suite['name']}_{i}",  # name
                             schema,  # schema
-                            case["condition"],  # condition
-                            case.get("context", [{}]),  # context
+                            case["patch"],  # patch
+                            case["payload"],  # payload
                             case["expected_result"],  # expected_result
                         )
                     )
     return parsed_tests
 
 
-@pytest.mark.parametrize(("name", "schema", "condition", "context", "expected_result"), _parse_tests())
-def test_all(name, schema, condition, context, expected_result):
+@pytest.mark.parametrize(("name", "schema", "patch", "payload", "expected_result"), _parse_tests())
+def test_all(name, schema, patch, payload, expected_result):
     raw_config = {
         "apiVersion": f"generic-webhook/{schema}",
         "kind": "GenericWebhookConfig",
-        "webhooks": [{"name": "test-webhook", "path": "test-path", "actions": [{"condition": condition}]}],
+        "webhooks": [{"name": "test-webhook", "path": "test-path", "actions": [{"patch": [patch]}]}],
     }
     gwcm = GenericWebhookConfigManifest(raw_config)
     action = gwcm.list_webhook_config[0].list_actions[0]
-    result = action.condition.get_value(context)
+    json_patch = action.get_patches(payload)
+    result = json_patch.apply(payload)
     assert result == expected_result
